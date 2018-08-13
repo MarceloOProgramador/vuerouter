255b952c19ab0d132aa6a7a40b75f558b8811cf7
Marcelo Pereira <marcelopereiraIFSP@gmail.com>
2018-08-13 16:48:25 -0300

    Added redirect route in routes

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,6 @@ import Products from "./Components/Products";
 export default [
   { path: "/", component: Home, name: "home"},
   { path: "/products/:id", component: Products, name: "products", props: true},
-  { path: "/tasks", component: Tasks, name: "tasks"}
+  { path: "/tasks", component: Tasks, name: "tasks"},
+  { path: "/redirect/", redirect: "products/10"}
 ]
