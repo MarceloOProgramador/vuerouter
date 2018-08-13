a64b1268b32a77e7563382ea09d745012ccb1498
Marcelo Pereira <marcelopereiraIFSP@gmail.com>
2018-08-13 16:57:35 -0300

    Created Error404Component and route Error404

---
+++ src/Components/Error404Component.vue
@@ -0,0 +1,15 @@
+<style scoped>
+
+</style>
+<template>
+  <h1>{{title}}</h1>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      "title" :  "Página não encontrada: Error 404",
+    }
+  }
+}
+</script>
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import Home from "./Components/Home";
 import Tasks from "./Components/Tasks";
 import Products from "./Components/Products";
+import Error404 from "./Components/Error404Component";
 
 export default [
   { path: "/", component: Home, name: "home"},
   { path: "/products/:id", component: Products, name: "products", props: true},
   { path: "/tasks", component: Tasks, name: "tasks"},
-  { path: "/redirect/", redirect: "products/10"}
+  { path: "/redirect/", redirect: "products/10"},
+  { path: "*", component: Error404}
 ]
